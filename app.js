26950b73a535e8629c0d69df8dc65ebd455b82ce
shafik720 <shafikrasel5@gmail.com>
2022-10-12 15:45:40 +0600

    sdfsf

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 
-const cardX = document.querySelector('.cardx'),
-img = cardX.querySelector('img');
+function random(min, max){
+    let x = Math.floor(Math.random() * (max-min+1)+min) ;
+    return x;
+    }
+let result = random(1, 6);
+console.log(result);
+
+let arr = [];
+
+for(let i=0; i<100;i++){
+    let x = random(1,6);
+    if(arr.indexOf(x) == -1){
+        arr.push(x);
+    }    
+}
+let arr2 = [];
+for(let i=0; i<100;i++){
+    let x = random(1,6);
+    if(arr2.indexOf(x) == -1){
+        arr2.push(x);
+    }    
+}
+let arr3 = [...arr2,...arr]
+console.log(arr3);
+
+const cardX = document.querySelectorAll('.cardx')
+// img = cardX.querySelector('img');
 
 
-// document.getElementById('card1').addEventListener('click',()=>{
-//     img.style.opacity = '1';
-// })
 
 function showImg(any){
     console.log(any.querySelector('img'));
     any.querySelector('img').style.opacity = '1';
 }
+
+for(let i=0;i<=cardX.length; i++){
+    
+    let div = document.createElement('div');
+    div.innerHTML = `<img src="assets/img/img (${arr3[i]}).jpg">`;
+    cardX[i].appendChild(div);
+    
+}
+
